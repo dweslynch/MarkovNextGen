29fda5b50136fe39ebde711a8aafd3d0969d8065
Devin <devin-lynch@uiowa.edu>
2018-03-07 23:32:49 -0600

    remove commented-out code

--- MarkovNextGen/MarkovNextGen/Link.fs
+++ MarkovNextGen/MarkovNextGen/Link.fs
@@ -18,32 +18,3 @@ type public Link =
                 let i = this.randy.Next(0, this.After.Count)
                 this.After.[i]
             else String.Empty
-
-(*
-type public Link(after : IEnumerable<string>) =
-    
-    let randy = new Random()
-    let mutable _after = new List<string> (after)
-
-    new() =
-        Link(new List<string>())
-
-    member public this.After
-        with get () = _after
-        and set (value) = _after <- value
-    
-    member public this.AddAfter(s) =
-        _after.Add(s)
-    
-    member public this.AddAfter(lst : IEnumerable<string>) =
-        for s in lst do
-            _after.Add(s)
-    
-    member public this.RandomAfter
-        with get () =
-            if _after.Count > 0 then
-                let i = randy.Next(0, _after.Count)
-                _after.[i]
-            else
-                String.Empty
-*)
